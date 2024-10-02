693e5255b1b9a4ba3794ba99943534ca010d19ab
ITRANSITION\m.trifonov <m.trifonov@itransition.com>
2024-10-02 10:16:22 +0400

    KYCForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Layout } from 'antd';
+import { BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
+import KYCFormContainer from './KYCFormContainer';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>      
+      <Layout className="layout">
+        <KYCFormContainer />
+      </Layout>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,90 @@
+import { forwardRef, useEffect, useImperativeHandle, useState } from 'react';
+import { Steps } from 'antd';
+import { useNavigate } from 'react-router-dom';
+
+const { Step } = Steps;
+
+const Navigation = forwardRef((_props, ref) => {
+  const [currentStep, setCurrentStep] = useState(0);
+  const navigate = useNavigate();
+
+  const incrementStep = () => {
+    setCurrentStep(currentStep + 1);
+  };
+
+  const decrementStep = () => {
+    setCurrentStep(currentStep - 1);
+  };
+
+  useImperativeHandle(ref, () => ({
+    incrementStep,
+    decrementStep,
+  }));
+
+  useEffect(() => {
+    navigateToStep(currentStep);
+  }, [currentStep]);
+
+  const handleStepChange = (step: number) => {
+    if (step > currentStep){
+        return;
+    }
+
+    setCurrentStep(step);
+  };
+
+  const navigateToStep = (step: number) => {
+    switch (step) {
+        case 0:
+          navigate('/personal-data');
+          break;
+        case 1:
+          navigate('/contact-data');
+          break;
+        case 2:
+          navigate('/household-data');
+          break;
+        case 3:
+          navigate('/employment-income');
+          break;
+        case 4:
+          navigate('/assets');
+          break;
+        case 5:
+          navigate('/liabilities');
+          break;
+        case 6:
+          navigate('/source-of-wealth');
+          break;
+        case 7:
+          navigate('/notes');
+          break;
+        case 8:
+          navigate('/documents');
+          break;
+        case 9:
+          navigate('/consents');
+          break;
+        default:
+          navigate('/step1');
+      }
+  }
+
+
+  return (
+    <Steps direction="vertical" current={currentStep} onChange={handleStepChange}>
+      <Step title="Personal data"/>
+      <Step title="Contact data"/>
+      <Step title="Household data"/>
+      <Step title="Employment/income"/>
+      <Step title="Assets and investments"/>
+      <Step title="Liabilities"/>
+      <Step title="Source of wealth"/>
+      <Step title="Notes"/>
+      <Step title="Documents"/>
+      <Step title="Consents"/>
+    </Steps>
+  );
+});
+
+export default Navigation;
